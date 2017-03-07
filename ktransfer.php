1967d6472d6e55c986f6f63fa6a6bbbe606c03a2
Luca <bulletti.luca@gmail.com>
2017-03-07 13:31:12 +0100

    fixed bug
    I fixed what you broke.

--- ktransfer.php
+++ ktransfer.php
@@ -1,28 +1,32 @@
 <?php
 	session_start();
 	$connection = mysqli_connect("localhost", "moriarty", "mogneHavcocoj", "moriarty_75");
-	$aok = true;
 	$sender = $_SESSION['name'];
-	$preceiver = $_POST['destination'];
+	$fuzzyReceiver = $_POST['destination'];
 	$amount = $_POST['karma'];
 	$comment = $_POST['comment'];
 	$private = isset($_POST['private']) ? 1 : 0;
+
 	// check if receiver is valid.
 	$query = "SELECT name FROM main WHERE name LIKE ?";
 	$stmt = mysqli_prepare($connection, $query);
-	$fuzzy = "%" . $preceiver . "%";
+	$fuzzy = "%" . $fuzzyReceiver . "%";
 	mysqli_stmt_bind_param($stmt, "s", $fuzzy);
 	mysqli_stmt_execute($stmt);
 	mysqli_stmt_store_result($stmt);
-	if (mysqli_stmt_num_rows($stmt) != 1) {
-		$aok = false;
+
+	if (mysqli_stmt_num_rows($stmt) == 1) {
+		$validReciever = true;
+	} else {
+		$validReciever = false;
 		$err = "Invalid user";
 	}
 	mysqli_stmt_bind_result($stmt, $receiver);
 	mysqli_stmt_fetch($stmt);
 	mysqli_stmt_close($stmt);
-	// check if amount is transferable.
-	if ($amount < 0 && $aok){
+
+	// check if amount is transferable
+	if ($amount < 0 && $validReciever){
 		$query = "SELECT amount FROM transactions WHERE sender=? AND receiver=?";
 		$stmt = mysqli_prepare($connection, $query);
 		mysqli_stmt_bind_param($stmt, "ss", $sender, $receiver);
@@ -30,24 +34,33 @@
 		mysqli_stmt_store_result($stmt);
 		mysqli_stmt_bind_result($stmt, $transferred);
 		if (mysqli_stmt_num_rows($stmt) > 0) {
-			$max_droppable = 0;
+			$maxDroppable = 0;
 			while (mysqli_stmt_fetch($stmt)) {
-				$max_droppable += $transferred;
+				$maxDroppable += $transferred;
 			}
-			if ($amount < 0 && ($max_droppable + $amount) < 0) { // negative values
-				$aok = false;
+			if ($amount < 0 && ($maxDroppable + $amount) < 0) {
+				$validKarma = false;
 				$err = "You can't take away karma you didn't give.";
+			} else {
+				$validKarma = true;
 			}
 		} else {
-			$aok = false;
+			$validKarma = false;
 			$err = "You can't take away karma you didn't give.";
 		}
 		mysqli_stmt_close($stmt);
 	} else if ($amount == 0) { // zero value
-		$aok = false;
+		$validKarma = false;
 		$err = "Invalid karma amount!";
+	} else if ($amount > $_SESSION['karma']) {
+		$validKarma = false;
+		$err = "Invalid karma amount!";
+	} else {
+		$validKarma = true;
 	}
-	if ($aok) {
+
+	// transfer karma after validity check
+	if ($validReciever && $validKarma) {
 		$query = "UPDATE main SET points= points + ? WHERE name=?";
 		$stmt = mysqli_prepare($connection, $query);
 		mysqli_stmt_bind_param($stmt, "is", $amount, $receiver);
@@ -62,7 +75,8 @@
 		mysqli_stmt_bind_param($stmt, "ssisi", $sender, $receiver, $amount, $comment, $private);
 		mysqli_stmt_execute($stmt);
 		mysqli_stmt_close($stmt);
-		$msg = array('success' => 'true', 'message' => 'Transaction successfull!', 'points' => $_SESSION['karma']);
+		$successmsg = 'Transaction successfull!';
+		$msg = array('success' => 'true', 'message' => $successmsg, 'points' => $_SESSION['karma']);
 		$jsonmsg = json_encode($msg);
 		echo $jsonmsg;
 	} else {
